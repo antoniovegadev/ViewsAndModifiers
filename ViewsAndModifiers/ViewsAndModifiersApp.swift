68e1ea07cac53e378d607b78dd23e7e0a1090e69
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-13 17:04:59 -0400

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Antonio Vega on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
